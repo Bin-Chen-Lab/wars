5b19f832a2b0b342a0cbd9630dfe725d4bfb5083
Bin Chen <binchenindiana@gmail.com>
2020-02-02 16:49:00 -0500

    add gene analysis

--- meta_gene.R
+++ meta_gene.R
@@ -1,58 +1,47 @@
 setwd("~/Documents/stanford/wars/cmap/data//")
-GSE17400 = read.csv("sars_GSE17400/dz_signature_all.txt", sep = "\t", stringsAsFactors = F)
-GSE22581 = read.csv("GSE22581/dz_signature_all_mapped.txt", sep = "\t", stringsAsFactors = F)
-GSE22581 = GSE22581[, colnames(GSE17400)]
-GSE30589 = read.csv("GSE30589/dz_signature_all.txt", sep = "\t", stringsAsFactors = F)
-GSE36016 = read.csv("GSE36016/dz_signature_all_mapped.txt", sep = "\t", stringsAsFactors = F)
-GSE36016 = GSE36016[, colnames(GSE17400)]
-GSE45042 = read.csv("GSE45042/dz_signature_all.txt", sep = "\t", stringsAsFactors = F)
-GSE47960 = read.csv("GSE47960/dz_signature_all.txt", sep = "\t", stringsAsFactors = F)
-GSE68820 = read.csv("GSE68820/dz_signature_all_mapped.txt", sep = "\t", stringsAsFactors = F)
-GSE68820 = GSE68820[, colnames(GSE17400)]
+
+meta_files = list.files(pattern =  "meta_")
+dz_signatures = NULL
+meta_files_valid = NULL
+for (meta_file  in meta_files){
+  if (file.exists(paste0(getwd(), "/", meta_file, "/sRGES_drugs.csv"))) {
+    dz_signature = read.table(paste0(getwd(), "/", meta_file, "/dz_signature_all.txt"), sep="\t", header=T)
+    
+    if (nrow(dz_signature) < 5) next
+    dz_signatures = rbind(dz_signatures, data.frame(name = meta_file, dz_signature))
+    meta_files_valid = c(meta_files_valid, meta_file)
+  }
+}
+
+dz_signatures_mock = (dz_signatures[grep("MOCK", dz_signatures$name), ])
+dz_signatures_cov = (dz_signatures[grep("SARS|MERS", dz_signatures$name), ])
+dz_signatures_mock = unique(dz_signatures_mock[dz_signatures_mock$up_down == "up", c("Symbol", "name")])
+dz_signatures_cov = unique(dz_signatures_cov[dz_signatures_cov$up_down == "up", c("Symbol", "name")])
+
+dz_signatures_cov_genes = table(dz_signatures_cov$Symbol)/length(unique(dz_signatures_cov$name))
+dz_signatures_mock_genes = table(dz_signatures_mock$Symbol)/length(unique(dz_signatures_mock$name))
+
+tail(sort(dz_signatures_cov_genes))
+tail(sort(dz_signatures_mock_genes))
+
+cov_genes_up = intersect(names(dz_signatures_cov_genes[dz_signatures_cov_genes > 0.5]),
+          names(dz_signatures_mock_genes[dz_signatures_mock_genes < 0.5]))
 
 
-all = data.frame(GSE = "GSE17400", type="human", GSE17400)
-all = rbind(all, data.frame(GSE = "GSE22581", type = "other", GSE22581))
-all = rbind(all, data.frame(GSE = "GSE30589", type = "human", GSE30589))
-all = rbind(all, data.frame(GSE = "GSE36016", type = "other", GSE36016))
-all = rbind(all, data.frame(GSE = "GSE45042", type = "human", GSE45042))
-all = rbind(all, data.frame(GSE = "GSE47960", type = "human", GSE47960))
-all = rbind(all, data.frame(GSE = "GSE68820", type = "other", GSE68820))
+dz_signatures_mock = (dz_signatures[grep("MOCK", dz_signatures$name), ])
+dz_signatures_cov = (dz_signatures[grep("SARS|MERS", dz_signatures$name), ])
+dz_signatures_mock = unique(dz_signatures_mock[dz_signatures_mock$up_down == "down", c("Symbol", "name")])
+dz_signatures_cov = unique(dz_signatures_cov[dz_signatures_cov$up_down == "down", c("Symbol", "name")])
 
-all$Symbol = sapply(all$Symbol, function(x) unlist(strsplit(x, " "))[1])
-all = unique(all[, -4])
+dz_signatures_cov_genes = table(dz_signatures_cov$Symbol)/length(unique(dz_signatures_cov$name))
+dz_signatures_mock_genes = table(dz_signatures_mock$Symbol)/length(unique(dz_signatures_mock$name))
 
-tail(sort(table(all$GeneID)), 10)
+tail(sort(dz_signatures_cov_genes))
+tail(sort(dz_signatures_mock_genes))
 
-genes = (table(all$GeneID))
-top_genes = top_genes[top_genes > 3]
+cov_genes_down = intersect(names(dz_signatures_cov_genes[dz_signatures_cov_genes > 0.5]),
+                      names(dz_signatures_mock_genes[dz_signatures_mock_genes < 0.5]))
 
-sum(genes > 4)
-all[all$GeneID == "3454",]
-#all[all$Symbol == "NPIPL3",]
+cov_genes = rbind(data.frame(gene = cov_genes_up, up_down = "up"), data.frame(gene = cov_genes_down, up_down = "down"))
 
-all_select = all[all$GeneID %in% names(genes[genes > 4]), ]
-all_select$direction = as.numeric(as.factor(all_select$up_down))
-all_select_collapse = aggregate(cbind(value, q.value, direction) ~ GeneID,  all_select, mean)
-all_select_collapse = all_select_collapse[all_select_collapse$direction == 2, ]
-gene_mapping = read.csv("raw/cmap/gene_info_hs.csv")[, c("GeneID", "Symbol")]
-all_select_collapse = merge(all_select_collapse, gene_mapping,by="GeneID")
-
-write.csv(all_select_collapse, "top_gene.csv")
-
-
-#H1NI
-GSE47960_H1N1 = read.csv("GSE47960_H1N1/dz_signature_all.txt", sep = "\t", stringsAsFactors = F)
-GSE17400_DOHV = read.csv("DOHV_GSE17400/dz_signature_all.txt", sep = "\t", stringsAsFactors = F)
-GSE17400_DOHV$Symbol = sapply(GSE17400_DOHV$Symbol, function(x) unlist(strsplit(x, " "))[1])
-GSE47960_H1N1$Symbol = sapply(GSE47960_H1N1$Symbol, function(x) unlist(strsplit(x, " "))[1])
-
-all_select_collapse_specific = all_select_collapse[!all_select_collapse$GeneID%in% 
-                                                     c(as.character(GSE17400_DOHV$GeneID), as.character(GSE47960_H1N1$GeneID)), ]
-
-all_select_collapse_specific = merge(all_select_collapse_specific, gene_mapping,by="GeneID")
-
-write.csv(all_select_collapse_specific, "top_gene_specific.csv")
-
-
-
+write.csv(cov_genes, "meta_cov_genes.csv")
